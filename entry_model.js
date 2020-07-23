d0d57e4f855a9d95b6595f094f0d15ee7afe13b4
Vince <vhamill@tamu.edu>
2020-07-22 22:47:54 -0500

    first commit

---
+++ entry_model.js
@@ -0,0 +1,49 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'elist_db',
+  password: 'postgres',
+  port: 5432,
+});
+
+const getEntries = () => {
+  return new Promise(function(resolve, reject) {
+    pool.query('SELECT * FROM entries.entries ORDER BY id ASC', (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(results.rows);
+    })
+  }) 
+}
+
+const createEntry = (body) => {
+  return new Promise(function(resolve, reject) {
+    const { title, medium, description, image_url } = body
+    pool.query('INSERT INTO entries.entries (title, medium, description, image_url) VALUES ($1, $2, $3, $4) RETURNING *', [title, medium, description, image_url], (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(`A new entry has been added added: ${results.rows[0]}`)
+    })
+  })
+}
+
+const deleteEntry = () => {
+  return new Promise(function(resolve, reject) {
+    const id = parseInt(request.params.id)
+    pool.query('DELETE FROM entries.entries WHERE id = $1', [id], (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(`Entry deleted with ID: ${id}`)
+    })
+  })
+}
+
+module.exports = {
+  getEntries,
+  createEntry,
+  deleteEntry,
+}
---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+const port = 3001
+
+const entry_model = require('./entry_model')
+
+app.use(express.json())
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+  next();
+});
+
+app.get('/', (req, res) => {
+  entry_model.getEntries()
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+app.post('/entries', (req, res) => {
+  entry_model.createEntry(req.body)
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+app.delete('/entries/:id', (req, res) => {
+  entry_model.deleteEntry(req.params.id)
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
+
